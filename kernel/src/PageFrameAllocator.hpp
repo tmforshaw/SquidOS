f3378470e5f2e49524b9fd0b3d3f1e1a18432689
Tyler Forshaw <tmforshaw@outlook.com>
2021-01-23 21:21:43 +0000

    Page Frame Allocator Implemented

---
+++ kernel/src/PageFrameAllocator.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "Bitmap.hpp"
+#include "EFI_Memory.hpp"
+#include "Memory.hpp"
+
+#include <stdint.h>
+
+class PageFrameAllocator
+{
+private:
+	void InitBitmap( size_t bitmapSize, void* bufferAddress );
+
+	void UnreservePage( void* address );
+	void ReservePage( void* address );
+
+	void UnreservePages( void* address, uint64_t pageCount );
+	void ReservePages( void* address, uint64_t pageCount );
+
+public:
+	Bitmap PageBitmap;
+
+	void FreePage( void* address );
+	void LockPage( void* address );
+
+	void FreePages( void* address, uint64_t pageCount );
+	void LockPages( void* address, uint64_t pageCount );
+
+	void ReadEFIMemoryMap( EFI_MEMORY_DESCRIPTOR* m_Map, size_t m_MapSize, size_t m_MapDescriptorSize );
+
+	void* RequestPage();
+
+	// Getters
+	uint64_t GetFreeRAM();
+	uint64_t GetUsedRAM();
+	uint64_t GetReservedRAM();
+};
--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,24 +1,70 @@
 #include "BasicRenderer.hpp"
+#include "Bitmap.hpp"
 #include "C_String.hpp"
 #include "EFI_Memory.hpp"
+#include "Memory.hpp"
+#include "PageFrameAllocator.hpp"
 
 struct BootInfo
 {
 	Framebuffer* framebuffer;
 	PSF1_FONT* psf1_font;
-	void* m_Map;
+	EFI_MEMORY_DESCRIPTOR* m_Map;
 	uint64_t m_MapSize;
 	uint64_t m_MapDescriptorSize;
 };
+
+extern uint64_t _KernelStart;
+extern uint64_t _KernelEnd;
 
 extern "C" void _start( BootInfo* bootInfo )
 {
 	BasicRenderer newRenderer = BasicRenderer( bootInfo->framebuffer, bootInfo->psf1_font );
 	newRenderer.CursorPosition = { 0, 0 };
 
+	uint64_t m_MapEntries = bootInfo->m_MapSize / bootInfo->m_MapDescriptorSize;
+
+	newRenderer.Print( "Total RAM: " );
+	newRenderer.Colour = 0xFFFF00FF;
+	newRenderer.Print( to_string( GetMemorySize( bootInfo->m_Map, m_MapEntries, bootInfo->m_MapDescriptorSize ) / 1024 ) );
+	newRenderer.Colour = 0xFF00FFFF;
+	newRenderer.Print( " KiB" );
 	newRenderer.CursorPosition = { 0, newRenderer.CursorPosition.Y + 16 }; // New Line
 
-	uint64_t m_MapEntries = bootInfo->m_MapSize / bootInfo->m_MapDescriptorSize;
+	PageFrameAllocator newAllocator;
+	newAllocator.ReadEFIMemoryMap( bootInfo->m_Map, bootInfo->m_MapSize, bootInfo->m_MapDescriptorSize );
+
+	// Lock the pages where the kernel is located
+	uint64_t kernelSize = ( uint64_t )( &_KernelEnd ) - ( uint64_t )( &_KernelStart );
+	uint64_t kernelPages = (uint64_t)kernelSize / 4096 + 1;
+	newAllocator.LockPages( &_KernelStart, kernelPages );
+
+	newRenderer.Print( "Free RAM: " );
+	newRenderer.Colour = 0xFFFF00FF;
+	newRenderer.Print( to_string( newAllocator.GetFreeRAM() / 1024 ) );
+	newRenderer.Colour = 0xFF00FFFF;
+	newRenderer.Print( " KiB" );
+	newRenderer.CursorPosition = { 0, newRenderer.CursorPosition.Y + 16 }; // New Line
+	newRenderer.Print( "Used RAM: " );
+	newRenderer.Colour = 0xFFFF00FF;
+	newRenderer.Print( to_string( newAllocator.GetUsedRAM() / 1024 ) );
+	newRenderer.Colour = 0xFF00FFFF;
+	newRenderer.Print( " KiB" );
+	newRenderer.CursorPosition = { 0, newRenderer.CursorPosition.Y + 16 }; // New Line
+	newRenderer.Print( "Reserved RAM: " );
+	newRenderer.Colour = 0xFFFF00FF;
+	newRenderer.Print( to_string( newAllocator.GetReservedRAM() / 1024 ) );
+	newRenderer.Colour = 0xFF00FFFF;
+	newRenderer.Print( " KiB" );
+	newRenderer.CursorPosition = { 0, newRenderer.CursorPosition.Y + 16 }; // New Line
+
+	for ( int t = 0; t < 5; t++ )
+	{
+		void* address = newAllocator.RequestPage();
+
+		newRenderer.Print( to_hstring( (uint64_t)address ) );
+		newRenderer.CursorPosition = { 0, newRenderer.CursorPosition.Y + 16 }; // New Line
+	}
 
 	for ( int i = 0; i < m_MapEntries; i++ )
 	{
@@ -28,7 +74,7 @@ extern "C" void _start( BootInfo* bootInfo )
 		newRenderer.Colour = 0xFFFF00FF;
 		newRenderer.Print( " " );
 		newRenderer.Print( to_string( desc->numPages * 4096 / 1024 ) ); // Print size in kb
-		newRenderer.Print( " KB" );
+		newRenderer.Print( " KiB" );
 		newRenderer.Colour = 0xFF00FFFF;
 	}
 
